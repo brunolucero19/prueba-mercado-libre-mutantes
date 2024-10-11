5a358139853c62e8dfb7f04a010a7c30b24586b1
brunolucero19 <brunolucero2419@gmail.com>
2024-10-10 22:23:14 -0300

    Validaciones de las secuencias añadidas.

--- src/main/java/com/example/parcialBrunoLucero50073/controllers/IndividuoController.java
+++ src/main/java/com/example/parcialBrunoLucero50073/controllers/IndividuoController.java
@@ -19,6 +19,14 @@ public class IndividuoController {
     }
     @PostMapping("/")
     public ResponseEntity<String> isMutant(@RequestBody DnaRequest dnaRequest){
+
+        try{
+            individuoService.validateDNASequence(dnaRequest.getDna());
+
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+
         try{
             boolean isMutant = individuoService.isMutant(dnaRequest.getDna());
             if (isMutant){
